22492458951cd733e5b7afa8822a751c9d3ffea1
dh0728 <dustkscjswo@naver.com>
2025-01-25 23:05:12 +0900

    git commit -m "[S12P11E206-148] [BE] Feat: ClimbInfo entity 관계설정 코드 추가"

---
+++ backend/src/main/java/com/project/backend/climbinfo/entity/ClimbInfo.java
@@ -0,0 +1,24 @@
+package com.project.backend.climbinfo.entity;
+
+import com.project.backend.climb.entity.Climb;
+import com.project.backend.info.entity.Info;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class ClimbInfo {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "climb_info_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "climb_id")
+    private Climb climb;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "info_id")
+    private Info info;
+}
