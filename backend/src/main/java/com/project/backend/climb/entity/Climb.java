4503cfbf6c5a0a6c159a4117b52424d133bac233
dh0728 <dustkscjswo@naver.com>
2025-01-27 23:03:25 +0900

    [S12P11E206-157] [BE] Feat: climb 전체 조회 정렬없이 조회 구현"

--- backend/src/main/java/com/project/backend/climb/entity/Climb.java
+++ backend/src/main/java/com/project/backend/climb/entity/Climb.java
@@ -4,33 +4,47 @@ import com.project.backend.climbinfo.entity.ClimbInfo;
 import com.project.backend.hold.entity.Hold;
 import com.project.backend.userclimb.entity.UserClimb;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Climb {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "climb_id")
     private Long id;
 
+
+    @Column(nullable = false)
     private String name;
 
+    @Lob
+    @Column(columnDefinition = "LONGTEXT")
     private String image;
 
+    @Column(nullable = false)
     private String address;
 
-    private BigDecimal longitude;
-
-    private BigDecimal latitude;
+    //이건 일일이 찾아넣기 귀찮은데..
+//    @Column(nullable = false)
+//    private BigDecimal longitude;
+//
+//    @Column(nullable = false)
+//    private BigDecimal latitude;
 
     private String open;
 
     private String number;
 
-
+    @Lob
+    @Column(columnDefinition = "LONGTEXT")
+    private String sns_url;
 
     @OneToMany(mappedBy = "climb", cascade = CascadeType.ALL)
     private List<UserClimb> userClimbList = new ArrayList<>();
