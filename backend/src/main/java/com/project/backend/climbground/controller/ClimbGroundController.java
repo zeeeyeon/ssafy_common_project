edd36e260280c3b9d927abd65157a84d07be0439
dh0728 <dustkscjswo@naver.com>
2025-01-29 02:42:44 +0900

    [S12P11E206-166] [BE] Feat: 클라이밍장 상세조회 기능구현

--- backend/src/main/java/com/project/backend/climbground/controller/ClimbGroundController.java
+++ backend/src/main/java/com/project/backend/climbground/controller/ClimbGroundController.java
@@ -1,12 +1,21 @@
 package com.project.backend.climbground.controller;
 
+import com.project.backend.climbground.dto.requsetDTO.ClimbGroundDetailRequestDTO;
 import com.project.backend.climbground.dto.responseDTO.ClimbGroundAllResponseDTO;
+import com.project.backend.climbground.dto.responseDTO.ClimbGroundDetailResponseDTO;
+import com.project.backend.climbground.repository.ClimbGroundRepository;
+import com.project.backend.climbground.service.ClimbGroundService;
 import com.project.backend.climbground.service.ClimbGroundServiceImpl;
+import com.project.backend.hold.dto.responseDTO.HoldResponseDTO;
+import com.project.backend.info.dto.responseDTO.InfoResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,6 +26,7 @@ public class ClimbGroundController {
     @Autowired
     private ClimbGroundServiceImpl ClimbGroundService;
 
+
     // 클라이밍장 리스트 조회
     @GetMapping("/all/user-location")
     public List<ClimbGroundAllResponseDTO> getALlCLimbs() {
@@ -26,31 +36,35 @@ public class ClimbGroundController {
     }
 
     // 클라이밍장 상세 조회
-//    @GetMapping("/detail/{climb_id}")
-//    public ResponseEntity<ClimbGroundDetailRequestDTO> getCLimbDetail(@PathVariable("climb_id") String climb_id) {
-//
-//    }
+    @GetMapping("/detail/{climbground_id}")
+    public ResponseEntity<?> getCLimbDetail(@PathVariable("climbground_id") Long climbground_id) {
+        return ClimbGroundService.findClimbGroundById(climbground_id)
+                .map(climbGround -> {
+                    List<HoldResponseDTO> holds = climbGround.getHoldList().stream()
+                            .sorted(Comparator.comparing(hold -> hold.getLevel().getValue()))
+                            .map(hold -> new HoldResponseDTO(hold.getId(), hold.getLevel(), hold.getColor()))
+                            .collect(Collectors.toList());
 
+                    List<InfoResponseDTO> infos = climbGround.getClimbGroundInfoList().stream()
+                            .map(info -> new InfoResponseDTO(info.getInfo().getInfo()))
+                            .collect(Collectors.toList());
 
-//    public List<ClimbResponseDTO> getAllClimbs() {
-//        return ClimbService.findAllClimb().stream()
-//                .map(climb -> {
-//                    List<HoldResponseDTO> holds = climb.getHoldList().stream()
-//                            .sorted(Comparator.comparing(hold -> hold.getLevel().getValue()))
-//                            .map(hold -> new HoldResponseDTO(hold.getId(), hold.getLevel(), hold.getColor()))
-//                            .collect(Collectors.toList());
-//
-//                    return new ClimbResponseDTO(
-//                            climb.getId(),
-//                            climb.getName(),
-//                            climb.getImage(),
-//                            climb.getAddress(),
-//                            climb.getOpen(),
-//                            climb.getNumber(),
-//                            climb.getSns_url()
-//                    );
-//                })
-//                .collect(Collectors.toList());
-//    }
+                    ClimbGroundDetailResponseDTO responseDTO = new ClimbGroundDetailResponseDTO(
+                            climbGround.getId(),
+                            climbGround.getName(),
+                            climbGround.getAddress(),
+                            climbGround.getNumber(),
+                            climbGround.getLatitude(),
+                            climbGround.getLongitude(),
+                            climbGround.getOpen(),
+                            climbGround.getSns_url(),
+                            holds,
+                            infos
+                    );
+                    return ResponseEntity.ok(responseDTO);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build()); // 찾는거 없으면 404에러
+
+    }
 
 }
--- backend/src/main/java/com/project/backend/climbground/repository/ClimbGroundRepository.java
+++ backend/src/main/java/com/project/backend/climbground/repository/ClimbGroundRepository.java
@@ -5,12 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface ClimbGroundRepository extends JpaRepository<ClimbGround, Long> {
 
-//    @Query("SELECT c from ClimbGround c " +
-//            "left join fetch c.holdList " + // 필요하다면 홀드도 함께 가져옵니다
-//            "left join fetch c.climbGroundInfoList ci " +
-//            "left join fetch ci.info " +
-//            "where c.id = :climbground_id")
-//    ClimbGround findClimbWithInfosById(@Param("climbground_id") Long climbground_id);
+    @Query("SELECT c from ClimbGround c " +
+            "left join fetch c.holdList " + // 필요하다면 홀드도 함께 가져옵니다
+            "left join fetch c.climbGroundInfoList ci " +
+            "left join fetch ci.info " +
+            "where c.id = :climbground_id")
+    Optional<ClimbGround> findClimbWithInfosById(@Param("climbground_id") Long climbground_id);
+
 }
