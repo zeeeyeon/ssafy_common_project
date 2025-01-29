b61f680b43d781843f0be24a6d78b2797706d6af
zeeeyeon <zeeyeonleee@gmail.com>
2025-01-29 13:19:52 +0900

    Refactor: api 주소 변경

--- backend/src/main/java/com/project/backend/record/controller/RecordController.java
+++ backend/src/main/java/com/project/backend/record/controller/RecordController.java
@@ -3,7 +3,7 @@ package com.project.backend.record.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/record")
+@RestController("/api/record")
 public class RecordController {
 
     @GetMapping()
