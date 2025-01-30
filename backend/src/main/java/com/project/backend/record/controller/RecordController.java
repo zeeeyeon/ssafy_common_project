ade304781f7816a8c92947680c0d33e470156e53
jiyeon_0342 <jiyeon_0342@naver.com>
2025-01-30 17:29:37 +0900

    3

--- backend/src/main/java/com/project/backend/record/controller/RecordController.java
+++ backend/src/main/java/com/project/backend/record/controller/RecordController.java
@@ -23,7 +23,7 @@ public class RecordController {
     @GetMapping("/api/record2")
     public String hello2(){
         List<ClimbGround> listAll = climbGroundRepository.findAll();
-        System.out.println("1111111");
+        System.out.println("11111111");
         return listAll.toString();
     }
 }
