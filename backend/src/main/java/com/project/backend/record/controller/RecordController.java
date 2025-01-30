9c845030e938921e34c72f49e6e60bea96718665
zeeeyeon <zeeyeonleee@gmail.com>
2025-01-30 17:36:16 +0900

    4

--- backend/src/main/java/com/project/backend/record/controller/RecordController.java
+++ backend/src/main/java/com/project/backend/record/controller/RecordController.java
@@ -23,7 +23,6 @@ public class RecordController {
     @GetMapping("/api/record2")
     public String hello2(){
         List<ClimbGround> listAll = climbGroundRepository.findAll();
-        System.out.println("11111111");
         return listAll.toString();
     }
 }
