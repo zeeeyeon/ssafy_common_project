11936b5499c71bed2d976f55a350752b8268b89a
zeeeyeon <zeeyeonleee@gmail.com>
2025-01-30 17:22:28 +0900

    a

--- backend/src/main/java/com/project/backend/record/controller/RecordController.java
+++ backend/src/main/java/com/project/backend/record/controller/RecordController.java
@@ -1,13 +1,28 @@
 package com.project.backend.record.controller;
 
+import com.project.backend.climbground.entity.ClimbGround;
+import com.project.backend.climbground.repository.ClimbGroundRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.webauthn.management.UserCredentialRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class RecordController {
+
+    private final ClimbGroundRepository climbGroundRepository;
 
     @GetMapping("/api/record")
     public String hello(){
         return "Hello, test code #1";
+    }
+
+    @GetMapping("/api/record2")
+    public String hello2(){
+        List<ClimbGround> listAll = climbGroundRepository.findAll();
+        return listAll.toString();
     }
 }
