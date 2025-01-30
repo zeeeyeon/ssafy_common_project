0d8c5e3869d9fdd5cc40a78a000a7a6661be4c10
dh0728 <dustkscjswo@naver.com>
2025-01-31 08:05:55 +0900

    [S12P11E206-166] [BE] Feat: 클라이밍장 검색기능_거리별 정렬 추가중

--- backend/src/main/java/com/project/backend/climbground/repository/ClimbGroundRepository.java
+++ backend/src/main/java/com/project/backend/climbground/repository/ClimbGroundRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,18 @@ public interface ClimbGroundRepository extends JpaRepository<ClimbGround, Long> 
 
     Optional<ClimbGround> findClimbGroundWithHoldById(Long climbground_id);
 
+
+    // 거리순으로 정렬 탐색
+    // HAVING distance < 10 일단 반경 내의 탐색 조건은 제거
+    @Query("SELECT c FROM ClimbGround c WHERE " +
+            "(6371 * acos(cos(radians(:latitude)) * cos(radians(c.latitude)) * " +
+            "cos(radians(c.longitude) - radians(:longitude)) + " +
+            "sin(radians(:latitude)) * sin(radians(c.latitude)))) " +
+            "ORDER BY distance")
+    List<ClimbGround> findAllClimbGroundWithDistance(
+            @Param("latitude") BigDecimal latitude,
+            @Param("longitude") BigDecimal longitude
+    );
 
 //    c.address or c.name에 문자열이 포함되어 있는 record 찾기
     @Query("SELECT c from ClimbGround c " +
