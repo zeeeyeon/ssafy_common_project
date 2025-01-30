2f0888ef33f097e54259bdca53281540f4acc2c4
jiyeon_0342 <jiyeon_0342@naver.com>
2025-01-30 16:02:19 +0900

    Refactor: docker-compose 파일 수정

--- backend/src/main/java/com/project/backend/record/controller/RecordController.java
+++ backend/src/main/java/com/project/backend/record/controller/RecordController.java
@@ -1,12 +1,13 @@
 package com.project.backend.record.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/record")
+@RestController
 public class RecordController {
 
-    @GetMapping()
+    @GetMapping("/api/record")
     public String hello(){
         return "Hello, test code #1";
     }
