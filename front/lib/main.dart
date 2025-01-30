b0042f3b278f48b498bc1b34ceab9ac430765908
김한민 <gksals9563@naver.com>
2025-01-31 02:22:06 +0900

    [S12P11E206-174] Design: 캘린더/상세페이지(클라이밍기록)구현

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -1,31 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kkulkkulk/common/api/dio_client.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:kkulkkulk/common/routes/app_router.dart';
+
+// Dio 인스턴스를 전역적으로 제공하는 provider
+final dioProvider = Provider((ref) => DioClient.getInstance());
 
 void main() {
   // Flutter 바인딩 초기화 추가
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
+  runApp(const ProviderScope(child: MyApp()));
 }
 
+// Equatable을 사용하여 위젯의 동등성 비교를 최적화
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: '끌락끌락',
+      title: '운동 기록 앱',
+      routerConfig: router,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ko', ''), // 한국어
+        Locale('en', ''), // 영어
+      ],
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
         fontFamily: 'Pretendard',
       ),
-      routerConfig: router,
       // 스플래시 화면 전에 보여질 화면 설정
       builder: (context, child) {
         return Scaffold(
