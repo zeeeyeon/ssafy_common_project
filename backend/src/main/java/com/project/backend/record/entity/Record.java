a9d64b40a443f9c22b6fa9867c0561de6a94e6ed
dh0728 <dustkscjswo@naver.com>
2025-02-01 04:07:09 +0900

    [S12P11E206-176] [BE] Refactor: BaseEntity 추가

--- backend/src/main/java/com/project/backend/record/entity/Record.java
+++ backend/src/main/java/com/project/backend/record/entity/Record.java
@@ -1,19 +1,23 @@
 package com.project.backend.record.entity;
 
+import com.project.backend.common.entity.BaseEntity;
 import com.project.backend.user.entity.User;
 import com.project.backend.hold.entity.Hold;
 import com.project.backend.userdate.entity.UserDate;
 import com.project.backend.video.entity.Video;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
-public class Record {
+@Getter
+@AttributeOverride(name="Id", column=@Column(name="record_id"))
+public class Record extends BaseEntity {
 
-    @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY )
-    @Column(name = "record_id")
-    private Long id;
-    private boolean isSuccess;
+//    @Id
+//    @GeneratedValue( strategy = GenerationType.IDENTITY )
+//    @Column(name = "record_id")
+//    private Long id;
+//    private boolean isSuccess;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_date_id")
