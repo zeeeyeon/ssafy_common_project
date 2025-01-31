ddfdfc47e1f8fca902f86d4e98ba8b0a600b1500
jiyeon_0342 <jiyeon_0342@naver.com>
2025-02-01 06:44:47 +0900

    Fix: test code

--- backend/src/main/java/com/project/backend/record/controller/RecordController.java
+++ backend/src/main/java/com/project/backend/record/controller/RecordController.java
@@ -23,6 +23,6 @@ public class RecordController {
     @GetMapping("/api/record2")
     public String hello2(){
         List<ClimbGround> listAll = climbGroundRepository.findAll();
-        return listAll.get(0).getName();
+        return listAll.get(1).getName();
     }
 }
