1b38dfef4187ac02dc624ae6be6abb2f25dc0463
jiyeon_0342 <jiyeon_0342@naver.com>
2025-02-01 06:54:44 +0900

    Fix: test code

--- backend/src/main/java/com/project/backend/record/controller/RecordController.java
+++ backend/src/main/java/com/project/backend/record/controller/RecordController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,9 +21,11 @@ public class RecordController {
         return "Hello, test code #1";
     }
 
-    @GetMapping("/api/record2")
-    public String hello2(){
+    public String hello2() {
         List<ClimbGround> listAll = climbGroundRepository.findAll();
-        return listAll.get(1).getName();
+        if (listAll.isEmpty()) {
+            return "No Data Found";
+        }
+        return Optional.ofNullable(listAll.get(1).getName()).orElse("Name is Null");
     }
 }
