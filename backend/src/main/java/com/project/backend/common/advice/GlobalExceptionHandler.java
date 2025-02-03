d636373f4a45955b54019f849e65cdf5380902bf
zeeeyeon <zeeyeonleee@gmail.com>
2025-02-03 17:19:06 +0900

    Feat: Exception, 예외처리 로직 구현

---
+++ backend/src/main/java/com/project/backend/common/advice/GlobalExceptionHandler.java
@@ -0,0 +1,69 @@
+package com.project.backend.common.advice;
+
+import com.project.backend.common.advice.dto.ExceptionContent;
+import com.project.backend.common.advice.exception.CustomException;
+import com.project.backend.common.response.Response;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.firewall.RequestRejectedException;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import static com.project.backend.common.response.ResponseCode.*;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = CustomException.class)
+    protected ResponseEntity<?> handleCustomException(CustomException exception) {
+        return new ResponseEntity<>(Response.create(exception.getResponseCode(), exception.getContent()),
+                exception.getResponseCode().getHttpStatus());
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class, BindException.class})
+    public ResponseEntity<?> bindingException(BindException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        String field = getFieldName(bindingResult);
+        String message = getDefaultMessage(bindingResult);
+
+        ExceptionContent content = new ExceptionContent(field, message);
+        return new ResponseEntity<>(Response.create(BINDING_ERROR, content), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({RequestRejectedException.class, HttpRequestMethodNotSupportedException.class, IllegalArgumentException.class, MethodArgumentTypeMismatchException.class})
+    public ResponseEntity<?> Exception(BindException e) {
+        return new ResponseEntity<>(Response.create(BAD_REQUEST, null), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException e) {
+        return new ResponseEntity<>(Response.create(ENTITY_NOT_FOUND, null), HttpStatus.NOT_FOUND);
+    }
+
+    private String getDefaultMessage(BindingResult bindingResult) {
+        String defaultMessage = "";
+        try {
+            defaultMessage = bindingResult.getFieldError().getDefaultMessage();
+        } catch (NullPointerException e) {
+        }
+        return defaultMessage;
+    }
+
+    private String getFieldName(BindingResult bindingResult) {
+        String fieldName = "";
+        try {
+            fieldName = bindingResult.getFieldError().getField();
+        } catch (NullPointerException e) {
+        }
+        return fieldName;
+    }
+
+
+}
