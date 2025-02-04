cbd8e92a708aeb4fd76fa40d08b646840edc3d8b
zeeeyeon <zeeyeonleee@gmail.com>
2025-02-04 16:26:10 +0900

    [BE] Test: code test

--- backend/src/main/java/com/project/backend/userdate/controller/UserDateController.java
+++ backend/src/main/java/com/project/backend/userdate/controller/UserDateController.java
@@ -31,7 +31,6 @@ public class UserDateController {
         return new ResponseEntity<>(Response.create(GET_DAILY_RECORD, dailyRecord), GET_DAILY_RECORD.getHttpStatus());
     }
 
-
     @GetMapping("/monthly/{userId}")
     public ResponseEntity<?> getMonthlyRecords(
             @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM") YearMonth selectedMonth,
