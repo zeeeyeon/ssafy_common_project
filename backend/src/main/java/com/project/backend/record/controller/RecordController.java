7114418900f10cb140b560182811bb30f9ad83f1
zeeeyeon <zeeyeonleee@gmail.com>
2025-02-05 17:10:01 +0900

    [BE] Test: test code

--- backend/src/main/java/com/project/backend/record/controller/RecordController.java
+++ backend/src/main/java/com/project/backend/record/controller/RecordController.java
@@ -1,13 +1,16 @@
 package com.project.backend.record.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class RecordController {
 
-    @GetMapping("/api/record")
-    public String hello(){
-        return "Hello, test code #1";
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("CORS 테스트 성공");
     }
 }
