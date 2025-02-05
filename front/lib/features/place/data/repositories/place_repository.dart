db9a159f489213a19e4843b02b73a42acf6292d2
seowoo <dkr0210@naver.com>
2025-02-05 18:05:55 +0900

    [S12P11E206-242] [FE] Feat: GPS 위치 연동

--- front/lib/features/place/data/repositories/place_repository.dart
+++ front/lib/features/place/data/repositories/place_repository.dart
@@ -1,1 +1,20 @@
- 
+import 'package:dio/dio.dart';
+import 'package:kkulkkulk/common/network/dio_client.dart';
+import 'package:kkulkkulk/features/place/data/models/place_all_model.dart';
+
+class PlaceRepository {
+  final Dio _dio = DioClient().dio;
+
+  // 클라이밍 장소 전체 조회(GPS 거리순)
+  Future<Response> getAllDisCLimbs(PlaceAllModel placeAllModel) async {
+    try {
+      final response = await _dio.get(
+        '/api/climbground/all/user-location',
+        data: placeAllModel.toJson(),
+      );
+      return response;
+    } catch(e) {
+      rethrow;
+    }
+  }
+}
