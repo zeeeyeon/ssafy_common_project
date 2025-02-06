bdf1ca0c0cbd24f6a9e009177015e8648798b1e5
dh0728 <dustkscjswo@naver.com>
2025-02-07 02:49:33 +0900

    [S12P11E206-241 ] [BE] Feat: s3 동영상 업로드 구현

--- backend/src/main/java/com/project/backend/record/controller/RecordController.java
+++ backend/src/main/java/com/project/backend/record/controller/RecordController.java
@@ -1,11 +1,17 @@
 package com.project.backend.record.controller;
 
+import com.project.backend.common.advice.exception.CustomException;
 import com.project.backend.common.response.Response;
+import com.project.backend.common.response.ResponseCode;
+import com.project.backend.record.dto.requestDTO.RecordSaveRequestDTO;
 import com.project.backend.record.service.VideoUploadService;
+import com.project.backend.video.service.S3UploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 import static com.project.backend.common.response.ResponseCode.SUCCESS_FILE_UPLOAD;
 
@@ -15,14 +21,24 @@ import static com.project.backend.common.response.ResponseCode.SUCCESS_FILE_UPLO
 public class RecordController {
 
 //    private final RecordService recordService;
+    private final S3UploadService s3UploadService;
     private final VideoUploadService videoUploadService;
 
     @PostMapping("/save")
-    public ResponseEntity<?> saveRecord(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<?> saveRecord(@RequestBody RecordSaveRequestDTO requestDTO) {
 
-        String filename = videoUploadService.upload(file);
+        if (requestDTO.getIsSuccess()){
 
-        return new ResponseEntity<>(Response.create(SUCCESS_FILE_UPLOAD, filename), SUCCESS_FILE_UPLOAD.getHttpStatus());
+        }
+
+        try {
+            String filename = s3UploadService.saveFile(requestDTO.getFile());
+            return new ResponseEntity<>(Response.create(SUCCESS_FILE_UPLOAD, filename), SUCCESS_FILE_UPLOAD.getHttpStatus());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new CustomException(ResponseCode.FILE_UPLOAD_FAILED);
+        }
+
     }
 
 }
