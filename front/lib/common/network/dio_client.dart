b5f6d4613330d81fde71a683e95e095232d7849a
DGW <won03289@gmail.com>
2025-02-07 17:01:01 +0900

    통계 페이지 연동중 ..

--- front/lib/common/network/dio_client.dart
+++ front/lib/common/network/dio_client.dart
@@ -1,3 +1,46 @@
+// import 'package:dio/dio.dart';
+
+// class DioClient {
+//   static final DioClient _instance = DioClient._internal();
+//   late final Dio dio;
+
+//   factory DioClient() {
+//     return _instance;
+//   }
+
+//   DioClient._internal() {
+//     dio = Dio(
+//       BaseOptions(
+//         baseUrl: 'https://i12e206.p.ssafy.io/', // API 서버 URL
+//         connectTimeout: const Duration(seconds: 5),
+//         receiveTimeout: const Duration(seconds: 3),
+//         headers: {
+//           'Content-Type': 'application/json',
+//           'Accept': 'application/json',
+//         },
+//       ),
+//     );
+
+//     // 인터셉터 설정
+//     dio.interceptors.add(
+//       InterceptorsWrapper(
+//         onRequest: (options, handler) {
+//           // 요청 전 처리
+//           return handler.next(options);
+//         },
+//         onResponse: (response, handler) {
+//           // 응답 처리
+//           return handler.next(response);
+//         },
+//         onError: (error, handler) {
+//           // 에러 처리
+//           return handler.next(error);
+//         },
+//       ),
+//     );
+//   }
+// }
+
 import 'package:dio/dio.dart';
 
 class DioClient {
@@ -11,30 +54,12 @@ class DioClient {
   DioClient._internal() {
     dio = Dio(
       BaseOptions(
-        baseUrl: 'https://api.kkulkkulk.com', // API 서버 URL
+        baseUrl: 'https://i12e206.p.ssafy.io', // API 기본 URL
         connectTimeout: const Duration(seconds: 5),
         receiveTimeout: const Duration(seconds: 3),
         headers: {
           'Content-Type': 'application/json',
           'Accept': 'application/json',
-        },
-      ),
-    );
-
-    // 인터셉터 설정
-    dio.interceptors.add(
-      InterceptorsWrapper(
-        onRequest: (options, handler) {
-          // 요청 전 처리
-          return handler.next(options);
-        },
-        onResponse: (response, handler) {
-          // 응답 처리
-          return handler.next(response);
-        },
-        onError: (error, handler) {
-          // 에러 처리
-          return handler.next(error);
         },
       ),
     );
