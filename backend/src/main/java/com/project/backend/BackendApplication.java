31ff9d10fe206332542728116c53642078a4a0ec
jiyeon_0342 <jiyeon_0342@naver.com>
2025-02-09 19:32:29 +0900

    [BE] Feat: RedisCacheConfig Bean 으로 등록

--- backend/src/main/java/com/project/backend/BackendApplication.java
+++ backend/src/main/java/com/project/backend/BackendApplication.java
@@ -2,7 +2,9 @@ package com.project.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class BackendApplication {
 
