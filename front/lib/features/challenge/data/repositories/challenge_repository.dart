8f138057d396eb6d54cffbf62f51f9b028e1c43e
seowoo <dkr0210@naver.com>
2025-02-10 17:49:36 +0900

    [S12P11E206-296] [FE] Feat: 챌린지 클라이밍장 해금 API 연동 구현

--- front/lib/features/challenge/data/repositories/challenge_repository.dart
+++ front/lib/features/challenge/data/repositories/challenge_repository.dart
@@ -4,7 +4,9 @@ import 'package:kkulkkulk/common/network/dio_client.dart';
 import 'package:kkulkkulk/features/challenge/data/models/challenge_all_model.dart';
 import 'package:kkulkkulk/features/challenge/data/models/challenge_response_model.dart';
 import 'package:kkulkkulk/features/challenge/data/models/detail_challenge_response_model.dart';
+import 'package:kkulkkulk/features/challenge/data/models/near_challenge_model.dart';
 import 'package:kkulkkulk/features/challenge/data/models/search_challenge_all_model.dart';
+import 'package:kkulkkulk/features/challenge/data/models/unlock_challenge_model.dart';
 
 class ChallengeRepository {
   final Dio _dio = DioClient().dio;
@@ -31,15 +33,54 @@ class ChallengeRepository {
   Future<List<ChallengeResponseModel>> searchGetAllChallenges(SearchChallengeAllModel searchhallengeAllModel) async {
     try {
       final response = await _dio.get(
-        'api/climbground/lock-glimbground/',
+        '/api/climbground/lock-climbground/search',
         queryParameters: {
           "userId": searchhallengeAllModel.userId,
+          "keyword": searchhallengeAllModel.keyword,
           "latitude": searchhallengeAllModel.latitude,
           "longitude": searchhallengeAllModel.longitude,
         }
       );
       List<dynamic> data = response.data['content'];
       return data.map<ChallengeResponseModel>((json) => ChallengeResponseModel.fromJson(json)).toList();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  // 해당 반경 안의 해금 가능한 챌린지 장소 조회
+  Future<ChallengeResponseModel> searchNearChallenges(NearChallengeModel nearChallengeModel) async {
+    try {
+      final response = await _dio.get(
+        '/api/climbground/lock-climbground/first',
+        queryParameters: {
+          "userId": nearChallengeModel.userId,
+          "latitude": nearChallengeModel.latitude,
+          "longitude": nearChallengeModel.longitude
+        }
+      );
+      ChallengeResponseModel data = ChallengeResponseModel.fromJson(response.data['content']);
+      return data;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  // 챌린지 장소 해금
+  Future<Response> unlockChallenge(UnlockChallengeModel unlockChallengeModel) async {
+    print(unlockChallengeModel);
+
+    try {
+      final response = await _dio.post(
+        '/api/record/unlock',
+        data: {
+          "userId": unlockChallengeModel.userId,
+          "climbGroundId": unlockChallengeModel.climbGroundId,
+          "latitude": unlockChallengeModel.latitude,
+          "longitude": unlockChallengeModel.longitude,
+        }
+      );
+      return response;
     } catch (e) {
       rethrow;
     }
@@ -59,4 +100,6 @@ class ChallengeRepository {
         rethrow;
       }
   }
+
+  
 }
