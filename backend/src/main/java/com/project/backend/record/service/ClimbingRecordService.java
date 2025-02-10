191510ab50ce5e920dbb6875c3f3281cee15f765
dh0728 <dustkscjswo@naver.com>
2025-02-11 02:12:57 +0900

    [S12P11E206-300] [BE] Fix: 나머지부분 userId 파라미터 -> authenticationPrincipal 변경

--- backend/src/main/java/com/project/backend/record/service/ClimbingRecordService.java
+++ backend/src/main/java/com/project/backend/record/service/ClimbingRecordService.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface ClimbingRecordService {
 
-    Optional<ClimbingRecord> saveRecord(RecordSaveRequestDTO requestDTO);
+    Optional<ClimbingRecord> saveRecord(Long userId, RecordSaveRequestDTO requestDTO);
 }
