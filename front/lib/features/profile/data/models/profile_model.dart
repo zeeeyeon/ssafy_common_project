c5dc659efcfc3bbc5a4a4baad421d27da23f5f91
DGW <won03289@gmail.com>
2025-02-11 16:41:59 +0900

    잠시 브랜치 변경

--- front/lib/features/profile/data/models/profile_model.dart
+++ front/lib/features/profile/data/models/profile_model.dart
@@ -1,37 +1,65 @@
-class ProfileModel {
-  final String nickname;
-  final String profileImage;
+class UserProfile {
+  final String username;
   final double height;
   final double armSpan;
-  final DateTime? climbingStartDate;
+  final String userTier;
+  final int dDay;
 
-  ProfileModel({
-    required this.nickname,
-    required this.profileImage,
+  UserProfile({
+    required this.username,
     required this.height,
     required this.armSpan,
-    this.climbingStartDate,
+    required this.userTier,
+    required this.dDay,
   });
 
-  factory ProfileModel.fromJson(Map<String, dynamic> json) {
-    return ProfileModel(
-      nickname: json['nickname'],
-      profileImage: json['profile_image'],
-      height: json['height'].toDouble(),
-      armSpan: json['arm_span'].toDouble(),
-      climbingStartDate: json['climbing_start_date'] != null
-          ? DateTime.parse(json['climbing_start_date'])
-          : null,
+  factory UserProfile.fromJson(Map<String, dynamic> json) {
+    return UserProfile(
+      username: json['username'],
+      height: (json['height'] as num).toDouble(),
+      armSpan: (json['armSpan'] as num).toDouble(),
+      userTier: json['userTier'],
+      dDay: json['dday'],
     );
   }
 
-  Map<String, dynamic> toJson() {
-    return {
-      'nickname': nickname,
-      'profile_image': profileImage,
-      'height': height,
-      'arm_span': armSpan,
-      'climbing_start_date': climbingStartDate?.toIso8601String(),
-    };
+  /// 클라이밍 티어에 따른 이미지 반환
+  String get tierImage {
+    switch (userTier) {
+      case "DIAMOND":
+        return "assets/images/tier/Diamond.webp";
+      case "PLATINUM":
+        return "assets/images/tier/Platinum.webp";
+      case "GOLD":
+        return "assets/images/tier/Gold.webp";
+      case "SILVER":
+        return "assets/images/tier/silver.webp";
+      case "BRONZE":
+        return "assets/images/tier/Bronze.webp";
+      default:
+        return "assets/images/tier/unranked.webp";
+    }
+  }
+
+  /// 티어 텍스트 반환
+  String get tierText {
+    switch (userTier) {
+      case "DIAMOND":
+        return "다이아몬드";
+      case "PLATINUM":
+        return "플래티넘";
+      case "GOLD":
+        return "골드";
+      case "SILVER":
+        return "실버";
+      case "BRONZE":
+        return "브론즈";
+      default:
+        return "비기너";
+    }
+  }
+
+  String get effectiveProfileImage {
+    return "assets/images/default_profile.png"; // 기본 이미지 설정
   }
 }
