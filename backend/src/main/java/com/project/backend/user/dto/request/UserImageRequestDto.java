3a8ce9f953d58b0894a839409b3a6397495918c6
jeong young han <daum4572@naver.com>
2025-02-11 17:01:05 +0900

    [S12P11E206-302] [BE] Refactor: 사용자 프로필 이미지 수정

--- backend/src/main/java/com/project/backend/user/dto/request/UserImageRequestDto.java
+++ backend/src/main/java/com/project/backend/user/dto/request/UserImageRequestDto.java
@@ -1,8 +1,9 @@
 package com.project.backend.user.dto.request;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class UserImageRequestDto {
-    private String profileImageUrl;
+    private MultipartFile profileImageUrl;
 }
