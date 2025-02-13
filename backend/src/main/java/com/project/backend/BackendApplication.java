efe4706588afeaa7cd82acbec57873ccff6c013f
zeeeyeon <zeeyeonleee@gmail.com>
2025-02-13 10:03:03 +0900

    Feat: 전역 시간 Asia/Seoul 설정

--- backend/src/main/java/com/project/backend/BackendApplication.java
+++ backend/src/main/java/com/project/backend/BackendApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import java.util.TimeZone;
+
 @EnableCaching
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
